840bde6eb84c3ac845381f7cb8536212f4cdd937
Jacobo Cohello <118378780+Jcooob@users.noreply.github.com>
2023-04-24 12:35:02 -0500

    AWS

--- src/db.js
+++ src/db.js
@@ -2,9 +2,11 @@ require('dotenv').config();
 const { Sequelize } = require('sequelize');
 const fs = require('fs');
 const path = require('path');
+const pg = require('pg');
 const { DB_USER, DB_PASSWORD, DB_HOST, DB_RAILWAY  } = process.env;
 
 const sequelize = new Sequelize(DB_RAILWAY , {
+  dialectModule: pg,
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
 });

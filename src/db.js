57efc7d09617e8a0551f2daaa6586b4c1ed53f2f
Jacobo Cohello <118378780+Jcooob@users.noreply.github.com>
2023-04-24 18:05:43 -0500

    Fix footer

--- src/db.js
+++ src/db.js
@@ -2,11 +2,11 @@ require('dotenv').config();
 const { Sequelize } = require('sequelize');
 const fs = require('fs');
 const path = require('path');
-// const pg = require('pg');
+const pg = require('pg');
 const { DB_AWS } = process.env;
 
 const sequelize = new Sequelize(DB_AWS , {
-  // dialectModule: pg,
+  dialectModule: pg,
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
 });
